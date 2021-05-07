dcf20d3818ea67a6620927dc095d50fbe271e9a7
patrikduksin <duksin2018@icloud.com>
2021-05-07 18:13:09 +0300

    init commit

---
+++ progressControl/progressControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  progressControl
+//
+//  Created by Patrik Duksin on 2021-05-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
