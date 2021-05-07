e8ad2c7e3589d49dabc08843fbb712804169e8d7
patrikduksin <duksin2018@icloud.com>
2021-05-08 02:24:52 +0300

    login page done

--- progressControl/progressControl/ViewController.swift
+++ progressControl/progressControl/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        hideKeyboardWhenTappedAround()
+        loginButton.layer.cornerRadius = 8
+        self.loginField.delegate = self
+        self.passwordField.delegate = self
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+                self.view.endEditing(true)
+                return false
+            }
 }
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
