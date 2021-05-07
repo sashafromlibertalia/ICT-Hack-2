2a30119e1a7a9ad6474821f25a6153b8cda5b5fc
Iskander <grinerplaydark@gmail.com>
2021-05-08 00:36:32 +0300

    new /home

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express'),
     path = require('path'),
     login = require('./routes/login'),
+    home = require('./routes/home'),
     db = require('./db')
 
 const app = express()
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/', login)
+app.use('/home', home)
 // app.use('/home', home)
 
 const PORT = process.env.PORT || 3000
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
 const {Router} = require('express')
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.render('home')
+router.get('/home', async (req, res) => {
+    res.render('home', {
+        title: 'Главная',
+        isCourses: true,
+    })
 })
-
 
 module.exports = router
