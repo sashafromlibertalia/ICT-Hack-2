c5a404bd91770bba6263f40dec38abefa2e85c47
tashareva <floydreme@yandex.ru>
2021-05-08 19:38:38 +0300

    created cards, via text(

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,46 @@
 const {Router} = require('express')
 const router = Router()
 const Time = new Date()
+const up = "#53F293"
+const down = "#FF1B51"
+
+let cards = [
+    {
+        header: "Выполненные задачи",
+        header_img: "url(/assets/doneCard.svg)",
+        text: "15",
+        arrow: "url(/assets/up-arrow.svg)",
+        stat_color: up,
+        stat_info: "15%",
+        stat_text: "с прошлой недели"
+    },
+    {
+        header: "Прошедшие дедлайны",
+        header_img: "url(/assets/unfinishedCard.svg)",
+        text: "2",
+        arrow: "url(/assets/up-arrow.svg)",
+        stat_color: up,
+        stat_info: "10%",
+        stat_text: "с прошлой недели"
+    },
+    {
+        header: "КПД",
+        header_img: "url(/assets/KPDCard.svg)",
+        text: "56%",
+        arrow: "url(/assets/down-arrow.svg)",
+        stat_color: down,
+        stat_info: "15%",
+        stat_text: "с прошлой недели"
+    },
+    {
+        header: "Ближайший дедлайн",
+        header_img: "url(/assets/calendarCard.svg)",
+        text: "09.05",
+        arrow: "",
+        stat_color: "",
+        stat_info: "",
+        stat_text: "..."
+    }]
 
 function greeting() {
     let text = ""
@@ -27,17 +67,17 @@ function greeting() {
     return text
 }
 
-
 router.post('/home', async (req, res) => {
     //req.session.isLogged = true;
     await res.redirect('/home')
 })
 
-router.get("/home", async(req, res) => {
+router.get("/home", async (req, res) => {
     await res.render("home", {
         isHome: true,
         message: greeting(),
-        title: "Главная"
+        title: "Главная",
+        cards: cards
     })
 })
 module.exports = router
